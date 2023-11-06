f8e70e5e5907a81ff1e5b5602500d7065f9296fa
ugiloitoshmirzaeva <ugiloitoshmirzaeva@gmail.com>
2023-11-06 18:58:27 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
+import Showcase from './Components/Showcase'
+import Shop from './Components/Shop'
+import Features from './Components/Features'
+import Highlightproducts from './Components/Highlightproduct'
+import FAQs from './Components/FAQs'
+import Footer from './Components/Footer'
 
 function App() {
   return(
     <>
+    <Showcase/>
+    <Shop/>
+    <Features/>
+    <Highlightproduct/>
+    <FAQs/>
+    <Footer/>
     </>
   )
 }
