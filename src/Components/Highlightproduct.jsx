6cf0fffef1ee2f4952cdf66151766475ceaadc78
ugiloitoshmirzaeva <ugiloitoshmirzaeva@gmail.com>
2023-11-06 15:01:28 +0500

    modified img of features section

--- src/Components/Highlightproduct.jsx
+++ src/Components/Highlightproduct.jsx
@@ -5,9 +5,7 @@ import Button from './Button'
 function Highlightproduct(params) {
     return(
         <div className="mainProduct">
-            <h1 >Room For</h1>
-            <h1 className="second">Fragrance</h1>
-         <img style={{height:'60vh', width:'65%', zIndex:'999'}} src="https://o.remove.bg/downloads/407dd04b-1186-4927-aaca-6b83bcf3ed8d/amber-glass-candle-jar-box-mockup_358694-4485-removebg-preview.png" alt="" />
+         <img style={{height:'60vh', width:'55%', zIndex:'999'}} src="https://i.ibb.co/zfY0RWY/Products.png" alt="" />
          <Button styleType="style1"  buttonText="FIND PRODUCT"></Button>
         </div>
     )
