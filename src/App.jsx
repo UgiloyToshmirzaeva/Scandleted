92b94882800131ae32d1a4a276e0fbbd73b54b8b
ugiloitoshmirzaeva <ugiloitoshmirzaeva@gmail.com>
2023-11-06 15:04:18 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css'
-import Features from './Components/Features'
-import Highlightproduct from './Components/Highlightproduct'
 
 function App() {
   return(
     <>
-    <Features />
-    <Highlightproduct />
     </>
   )
 }
